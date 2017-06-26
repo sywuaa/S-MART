c28f5b598db1d1de8e069349677ce78e94b60a35
Shao <sywu.us@gmail.com>
2017-06-26 11:00:49 -0400

    updated seed

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,7 +2,7 @@ class Api::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save!
+    if @user.save
       log_in(@user)
       render 'api/sessions/show'
     else

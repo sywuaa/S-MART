1ee97fbe5f45b18ec8617348ef298110bf92733a
Shao <sywu.us@gmail.com>
2017-06-30 00:26:34 -0400

    heroku

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module FSProject
       :storage => :s3,
       :path => "/:class/:attachment/:id_partition/:style/:filename",
       :s3_credentials => {
-        :bucket => ENV["s3_bucket"],
+        :bucket_name => ENV["s3_bucket"],
         :access_key_id => ENV["s3_access_key_id"],
         :secret_access_key => ENV["s3_secret_access_key"],
         :s3_region => ENV["s3_region"],

bcce0ef2e233dd0cea5cf5b78b360ec04c3a5682
Shao <sywu.us@gmail.com>
2017-06-30 00:24:12 -0400

    heroku

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module FSProject
     # -- all .rb files in that directory are automatically loaded.
     config.paperclip_defaults = {
       :storage => :s3,
-      path: ‘/:class/:attachment/:id_partition/:style/:filename’
+      :path => "/:class/:attachment/:id_partition/:style/:filename",
       :s3_credentials => {
         :bucket => ENV["s3_bucket"],
         :access_key_id => ENV["s3_access_key_id"],

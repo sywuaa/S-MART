0233dd151259b5e000e44bc5e4e3d89e61bc79ea
Shao <sywu.us@gmail.com>
2017-09-28 16:51:18 -0400

    testing receive error in campaign action

--- frontend/actions/campaign_actions.js
+++ frontend/actions/campaign_actions.js
@@ -3,6 +3,7 @@ export const RECEIVE_ALL_CAMPAIGNS = 'RECEIVE_ALL_CAMPAIGNS';
 export const UPDATE_CAMPAIGN = 'UPDATE_CAMPAIGN';
 
 import * as APIUtil from '../util/campaign_api_util';
+import { receiveErrors, clearErrors } from './error_actions';
 
 export const createCampaign = (formData) => dispatch => {
   return(
@@ -21,7 +22,8 @@ export const fetchCampaign = (id) => dispatch => {
 export const fetchAllCampaigns = () => dispatch => {
   return(
     APIUtil.fetchAllCampaigns()
-      .then( campaigns => dispatch(receiveAllCampaigns(campaigns)))
+      .then( campaigns => dispatch(receiveAllCampaigns(campaigns)),
+             errors => dispatch(receiveErrors(errors.responseJSON)))
   );
 };
 

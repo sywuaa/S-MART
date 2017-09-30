c0c32b951ab72121487a41b851868f5e98f428a8
Shao <sywu.us@gmail.com>
2017-09-29 23:10:57 -0400

    successful importing error handling for campaign creation

--- frontend/actions/campaign_actions.js
+++ frontend/actions/campaign_actions.js
@@ -1,11 +1,9 @@
 export const RECEIVE_CAMPAIGN = 'RECEIVE_CAMPAIGN';
 export const RECEIVE_ALL_CAMPAIGNS = 'RECEIVE_ALL_CAMPAIGNS';
 export const UPDATE_CAMPAIGN = 'UPDATE_CAMPAIGN';
-export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
-export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
 
 import * as APIUtil from '../util/campaign_api_util';
-// import { receiveErrors, clearErrors } from './error_actions';
+import { receiveErrors, clearErrors, RECEIVE_ERRORS, CLEAR_ERRORS } from './error_actions';
 
 export const createCampaign = (formData) => dispatch => {
   debugger
@@ -48,18 +46,5 @@ export const receiveAllCampaigns = (payload) => {
   return {
     type: RECEIVE_ALL_CAMPAIGNS,
     payload
-  };
-};
-
-export const receiveErrors = errors => {
-  return {
-    type: RECEIVE_ERRORS,
-    errors
-  };
-};
-
-export const clearErrors = () => {
-  return {
-    type: CLEAR_ERRORS
   };
 };

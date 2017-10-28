1bd8ab253c7dc0c908214c364632821ba3b44b57
Shao <sywu.us@gmail.com>
2017-10-27 23:44:45 -0400

    creating parent state

--- frontend/components/body/caro.jsx
+++ frontend/components/body/caro.jsx
@@ -1,2 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+class Caro extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      direction: 'left',
+      campaigns: [],
+      left: 1,
+      center: 2,
+      right: 3
+    };
+
+  }
+}

d368efd479a5cf265139c3bfd4493647f5ce40ad
Shao <sywu.us@gmail.com>
2017-11-04 23:57:21 -0400

    testing button

--- frontend/components/body/carousel/center_caro.jsx
+++ frontend/components/body/carousel/center_caro.jsx
@@ -6,12 +6,18 @@ class CenterCaro extends React.Component{
   constructor(props){
     super(props);
 
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(id) {
+    this.props.history.push(`/campaigns/${id}`);
   }
 
   render(){
 
     if (this.props.campaign){
-      this.img = (<img key={this.props.campaign.id} src={this.props.campaign.carousel_image} />);
+      this.img = (<img key={this.props.campaign.id} src={this.props.campaign.carousel_image}>
+        </img> );
     }else {
       this.img = "No Image";
     }

6bd351657aeef91ac72d6eb35931fef789f0e239
Shao <sywu.us@gmail.com>
2018-05-14 17:24:57 -0400

    config

--- config/application.rb
+++ config/application.rb
@@ -9,12 +9,6 @@ Bundler.require(*Rails.groups)
 module FSProject
   class Application < Rails::Application
 
-    config.middleware.insert_before 0, "Rack::Cors" do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
